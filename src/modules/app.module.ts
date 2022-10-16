a650305283659503c69aef88d6f9469faf799f8e
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-10-16 11:06:03 -0300

    fix: app.service and app.controller removed from app.module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '../app.controller';
-import { AppService } from '../app.service';
 import { SystemModule } from './system/system.module';
 
 @Module({
   imports: [SystemModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
